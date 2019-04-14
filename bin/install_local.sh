1ade766443d36e374f8aa08908581cfb65c410c6
qinweijunlin <qinweijunlin@qq.com>
2019-04-14 18:38:21 +0800

    发布

--- bin/install_local.sh
+++ bin/install_local.sh
@@ -5,12 +5,33 @@ set -euo pipefail
 
 #### 常量 ####
 readonly ROOTDIR='/yzaj'
+readonly E_NOT_FOUND=127
 
 #### 包含 ####
 
 #### 函数 ####
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
 
 #### 变量 ####
+readonly oneself="$0"
 readonly reposrc="${ROOTDIR}/remote-resource/resource"
 
 #### 主体 ####
+if [[ ! -d "${reposrc}" ]]; then
+  err "${oneself##*/}: remote-resource not found"
+  exit "${E_NOT_FOUND}"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
