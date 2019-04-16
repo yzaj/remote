28d6d6b0c58d5602d3b2c29c35d6c3037d2cc9d3
qinweijunlin <qinweijunlin@qq.com>
2019-04-16 18:30:37 +0800

    发布

--- bin/remotevar.sh
+++ bin/remotevar.sh
@@ -3,18 +3,11 @@
 # 常量与变量
 set -euo pipefail
 
+# 常量 - 退出状态码
+
+# 变量 - remote.conf
+qq_list='3-1,3-2,4-1,4-2,5-1,5-2,6-1,6-2,7-1,7-2'
+
 # 变量 - remote.sh
+readonly confdir="${rootdir}/remote/conf"
 readonly tempdir="${rootdir}/temp/remote"
-
-
-
-
-
-
-
-
-
-
-
-
-
