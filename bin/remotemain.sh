def971b8fd6799f1ddc3debbf859a9c41c32f0f4
qinweijunlin <qinweijunlin@qq.com>
2019-04-18 07:50:33 +0800

    发布

--- bin/remotemain.sh
+++ bin/remotemain.sh
@@ -14,6 +14,7 @@ fi
 
 cp -r "${condir}"/upload "${tempdir}"
 
+for qqbatch in ${qqbatchs}; do
 
 
 
@@ -21,74 +22,81 @@ cp -r "${condir}"/upload "${tempdir}"
 
 
 
+  case "${qq}" in
+    all)
+      cpall
+      ;;
+    1-1)
+      cpbatch 1 "${qq}"
+      ;;
+    1-2)
+      cpbatch 2 "${qq}"
+      ;;
+    2-1)
+      cpbatch 3 "${qq}"
+      ;;
+    2-2)
+      cpbatch 4 "${qq}"
+      ;;
+    3-1)
+      cpbatch 5 "${qq}"
+      ;;
+    3-2)
+      cpbatch 6 "${qq}"
+      ;;
+    4-1)
+      cpbatch 7 "${qq}"
+      ;;
+    4-2)
+      cpbatch 8 "${qq}"
+      ;;
+    5-1)
+      cpbatch 9 "${qq}"
+      ;;
+    5-2)
+      cpbatch 10 "${qq}"
+      ;;
+    6-1)
+      cpbatch 11 "${qq}"
+      ;;
+    6-2)
+      cpbatch 12 "${qq}"
+      ;;
+    7-1)
+      cpbatch 13 "${qq}"
+      ;;
+    7-2)
+      cpbatch 14 "${qq}"
+      ;;
+    8-1)
+      cpbatch 15 "${qq}"
+      ;;
+    8-2)
+      cpbatch 16 "${qq}"
+      ;;
+    9-1)
+      cpbatch 17 "${qq}"
+      ;;
+    9-2)
+      cpbatch 18 "${qq}"
+      ;;
+    10-1)
+      cpbatch 19 "${qq}"
+      ;;
+    10-2)
+      cpbatch 20 "${qq}"
+      ;;
+    *)
+      :
+      ;;
+  esac
 
-case "${qq}" in
-  all)
-    cpall
-    ;;
-  1-1)
-    cpbatch 1 "${qq}"
-    ;;
-  1-2)
-    cpbatch 2 "${qq}"
-    ;;
-  2-1)
-    cpbatch 3 "${qq}"
-    ;;
-  2-2)
-    cpbatch 4 "${qq}"
-    ;;
-  3-1)
-    cpbatch 5 "${qq}"
-    ;;
-  3-2)
-    cpbatch 6 "${qq}"
-    ;;
-  4-1)
-    cpbatch 7 "${qq}"
-    ;;
-  4-2)
-    cpbatch 8 "${qq}"
-    ;;
-  5-1)
-    cpbatch 9 "${qq}"
-    ;;
-  5-2)
-    cpbatch 10 "${qq}"
-    ;;
-  6-1)
-    cpbatch 11 "${qq}"
-    ;;
-  6-2)
-    cpbatch 12 "${qq}"
-    ;;
-  7-1)
-    cpbatch 13 "${qq}"
-    ;;
-  7-2)
-    cpbatch 14 "${qq}"
-    ;;
-  8-1)
-    cpbatch 15 "${qq}"
-    ;;
-  8-2)
-    cpbatch 16 "${qq}"
-    ;;
-  9-1)
-    cpbatch 17 "${qq}"
-    ;;
-  9-2)
-    cpbatch 18 "${qq}"
-    ;;
-  10-1)
-    cpbatch 19 "${qq}"
-    ;;
-  10-2)
-    cpbatch 20 "${qq}"
-    ;;
-  *)
-    :
-    ;;
-esac
+
+
+
+
+
+
+done
 
 
