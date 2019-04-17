bdf24348a55c85f566a7bec3daf27f366071d091
qinweijunlin <qinweijunlin@qq.com>
2019-04-17 09:40:06 +0800

    发布

--- bin/remotearg.sh
+++ bin/remotearg.sh
@@ -6,7 +6,14 @@ set -euo pipefail
 # 范例: remote.sh all 1,2,3
 #       remote.sh 3-1 1,2,3
 
-
+if [[ "${qq}" == "all" ]]; then
+  :
+elif [[ "${qq}" =~ ^[1-9]0{0,1}-[1-2]$ && -d "${condir}/slave-${qq}" ]] && echo "${qq_list}" | grep ",${qq},"; then
+  :
+else
+  err "remote.sh: unrecognized option '${qq}'"
+  exit "${E_UNRECOGNIZED_OPTION}"
+fi
 
 
 
