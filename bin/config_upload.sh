9600abbb8a1712d267da65a471d3431fd91a4f46
qinweijunlin <qinweijunlin@qq.com>
2019-04-24 11:48:39 +0800

    发布

--- bin/config_upload.sh
+++ bin/config_upload.sh
@@ -8,7 +8,7 @@ readonly Q5_2='1 2 3 4 18 19 20 21 22 23 24 25 26 27 28 29 30 33 34 35 36'
 readonly Q6_2='1 2 3 4 18'
 readonly Q7_2='1 2 3 4 18'
 
-if [[ "${qq}" == "5-2" ]]; then
+if [[ "${qq}" == "5-2" || "${qq_all}" == "5-2" ]]; then
   for q in ${Q5_2}; do
     
     bash "${rootdir}"/shell/bin/sed.sh "^设置_启动游戏角色=.*" "设置_启动游戏角色=2" "${updir}/set${q}.ini"
@@ -16,7 +16,7 @@ if [[ "${qq}" == "5-2" ]]; then
   done
 fi
 
-if [[ "${qq}" == "6-2" ]]; then
+if [[ "${qq}" == "6-2" || "${qq_all}" == "6-2" ]]; then
   for q in ${Q6_2}; do
     
     bash "${rootdir}"/shell/bin/sed.sh "^设置_启动游戏角色=.*" "设置_启动游戏角色=2" "${updir}/set${q}.ini"
@@ -24,7 +24,7 @@ if [[ "${qq}" == "6-2" ]]; then
   done
 fi
 
-if [[ "${qq}" == "7-2" ]]; then
+if [[ "${qq}" == "7-2" || "${qq_all}" == "7-2" ]]; then
   for q in ${Q7_2}; do
     
     bash "${rootdir}"/shell/bin/sed.sh "^设置_启动游戏角色=.*" "设置_启动游戏角色=2" "${updir}/set${q}.ini"
